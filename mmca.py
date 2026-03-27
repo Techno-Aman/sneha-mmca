7113389bc8fb89132c89a16554fa65c493dd317e
Aman_Tech <144775154+Techno-Aman@users.noreply.github.com>
2026-03-27 16:51:29 +0530

    added code explaination page

--- mmca.py
+++ mmca.py
@@ -11,7 +11,7 @@ if page == "Model" :
     st.sidebar.header("⚙️ Input Parameters")
 
     arrival_rate = st.sidebar.number_input("Arrival Rate (λ)", min_value=0.1, max_value=12.0, value=12.0)
-    service_rate = st.sidebar.number_input("Service Rate (μ)", min_value=0.1, max_value=5,value=5.0)
+    service_rate = st.sidebar.number_input("Service Rate (μ)", min_value=0.1, max_value=5.0,value=5.0)
     doctors = st.sidebar.number_input("Number of Doctors (c)", 1, 20, 2)
     max_doctors = st.sidebar.number_input("Max Doctors for Graph", 1, 20, 2)
 
