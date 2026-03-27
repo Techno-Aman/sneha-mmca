571f5e1d3cfe0e4eb8c450607e57e9efe7da345d
Aman_Tech <144775154+Techno-Aman@users.noreply.github.com>
2026-03-27 16:49:59 +0530

    added code explaination page

--- mmca.py
+++ mmca.py
@@ -10,10 +10,10 @@ if page == "Model" :
     # Sidebar Inputs
     st.sidebar.header("⚙️ Input Parameters")
 
-    arrival_rate = st.sidebar.number_input("Arrival Rate (λ)", min_value=0.1, max_value=12.0)
-    service_rate = st.sidebar.number_input("Service Rate (μ)", min_value=0.1, max_value=5.0)
-    doctors = st.sidebar.number_input("Number of Doctors (c)", 1, 20, 1)
-    max_doctors = st.sidebar.number_input("Max Doctors for Graph", 1, 20, 1)
+    arrival_rate = st.sidebar.number_input("Arrival Rate (λ)", min_value=0.1, max_value=12.0, value=12)
+    service_rate = st.sidebar.number_input("Service Rate (μ)", min_value=0.1, max_value=value=12)
+    doctors = st.sidebar.number_input("Number of Doctors (c)", 1, 20, 2)
+    max_doctors = st.sidebar.number_input("Max Doctors for Graph", 1, 20, 2)
 
     # Function to calculate P0
     def calculate_p0(lam, mu, c):
